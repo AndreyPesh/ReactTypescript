e76a45837c60d6733dc4887aaf0349fd2eaf94a4
Andrey Peshko <coursestud@mail.ru>
2022-02-03 21:52:19 +0300

    feat: add base webpack

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import React, { useEffect } from 'react';
+import image from './assets/images/Mona.jpg';
+
+const styles: React.CSSProperties = {
+  width: '18rem',
+};
+
+const Component = (props: TypeProps) => {
+  return <div>
+    {props.name}
+    <div className='card' style={styles}>
+  <img src={image} className='card-img-top' alt='...' />
+  <div className='card-body'>
+    <p className='card-title'>Card title</p>
+    <p className='card-text'>Some quick example text to build on the card title and make up the bulk of the cards content.</p>
+    <a href='#' className='btn btn-primary'>Go somewhere</a>
+  </div>
+</div>
+  </div>;
+};
+
+export const App = () => {
+  useEffect(() => {
+  });
+  return <div>
+    <img src={image} />
+    <button className='btn btn-primary'>Primary</button>
+    </div>;
+};
+
+type TypeProps = {
+  name: string
+};
